d965cc7ab8c9f43e0e7b6167a5baaed548626454
Oshalusi John <48496855+prof001@users.noreply.github.com>
2020-03-14 20:33:08 +0100

    Few more works on uploading data

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,32 @@
 const { Router } = require('express');
+const multer = require('multer');
 
 const db = require('../db/index');
 
 const router = Router();
+
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimeType];
+    let error = new Error('Invalid mime type');
+    if (isValid) {
+      error = null;
+    }
+    cb(null, "./public/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimeType];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+  }
+});
 
 db.connect((err) => {
   if(err) throw err;
@@ -21,8 +45,9 @@ router.get('/', (request, response) => {
   });
 });
 
-router.post('/addPost', (request, response) => {
-  const { title, content } = request.body;
+
+router.post('/addPost', multer({storage: storage}).single('image'), (request, response) => {
+ /*const { title, content } = request.body;
 
   db.query('insert into posts (title, content) values (?, ?)',
     [title, content],
@@ -33,9 +58,25 @@ router.post('/addPost', (request, response) => {
         (err2, res2) => {
           if (err2) throw err2;
 
-          response.status(200).send(res2);
+          response.status(201).send(res2);
         });
+    });*/
+ console.log(request.body);
+});
+
+router.put('/updatePost/:postId', (request, response) => {
+  const { postId } = request.params;
+  const { title, content } = request.body;
+
+  db.query(
+    'update posts set title = ?, content = ? where id = ?',
+    [title, content, postId],
+    (err, res) => {
+      if (err) throw err;
+
+      response.status(200).send({message: 'Post updated successfully'});
     });
+  // console.log(request.body);
 });
 
 router.delete('/deletePost/:postId', (request, response) => {
